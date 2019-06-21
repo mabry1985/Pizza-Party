7b6e3ece307160694f8d7daebd42ca4eb9b7c192
mabry1985 <mabry1985@gmail.com>
2019-06-21 15:03:25 -0700

    rework logic for adding pizza total

--- js/scripts.js
+++ js/scripts.js
@@ -7,58 +7,33 @@ Orders.prototype.addPizza = function (pizza) {
   this.order.push(pizza);
 };
 
-function Pizza(size) {
-  this.size = size;
+function Pizza() {
   this.toppings = [];
 };
 
-function Meat(name) {
-  this.name = name;
-  this.cost = 1;
-}
-
-function Veggie() {
-  this.cost = .5;
-}
-
-function Novelty() {
-  this.cost = 5;
-}
-
-Pizza.prototype.addToppings = function (topping) {
-  this.toppings.push(topping);
-};
-
-var small = 10;
-var medium = 12;
-var large = 14;
-var cheese = new Meat('Extra Cheese');
-var sauce = new Veggie('Extra Sauce');
-var pepperoni = new Meat('Pepperoni');
-var sausage = new Meat('Sausage');
-var ham = new Meat('Ham');
-var onion = new Veggie('Onions');
-var redPepper = new Veggie('Red Peppers');
-var mushroom = new Veggie('Mushrooms');
-var garlic = new Veggie('Crushed Garlic');
-var pineapple = new Veggie('Pineapple');
-var jalepeno = new Veggie('Jalepeno');
-var tomato = new Veggie('Tomatoes');
-
-var order = new Orders('Josh');
-var pizza1 = new Pizza(large);
+var order1 = new Orders('Josh');
+var pizza1 = new Pizza();
+order1.addPizza(pizza1);
 
 $(document).ready(function () {
   $('.form-one').submit(function (event) {
     event.preventDefault();
     var nameInput = $('input.name').val();
-    var sizeInput = $('input:radio[name=size]:checked').val();
-    console.log(nameInput);
-    console.log(sizeInput);
+    var sizeInput = parseInt($('input:radio[name=size]:checked').val());
+    toppingsArr = [];
     $('input:checkbox[name=topping]:checked').each(function () {
-        var toppingInput = $(this).val();
-
-        console.log(toppingInput);
+        var toppingInput = parseInt($(this).val());
+        toppingsArr.push(toppingInput);
       });
+
+    var total = sizeInput;
+
+    toppingsArr.forEach(function (topping) {
+      total += topping;
+    });
+
+
+    console.log(total);
+    console.log(toppingsArr);
   });
 });
