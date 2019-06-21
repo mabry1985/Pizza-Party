e2d6baacfaf883b5911bb627c03aed6a0d8d94ad
mabry1985 <mabry1985@gmail.com>
2019-06-21 11:41:08 -0700

    add topping objects to global scope and begin html form

--- js/scripts.js
+++ js/scripts.js
@@ -12,22 +12,34 @@ function Pizza(size) {
   this.toppings = [];
 };
 
+function Meat() {
+  this.cost = 1;
+}
+
+function Veggie() {
+  this.cost = .5;
+}
+
+function Novelty() {
+  this.cost = 5;
+}
+
 Pizza.prototype.addToppings = function (topping) {
   this.toppings.push(topping);
 };
 
-
-
-
-
-
-
-
-
-
-
+var small = 10;
+var medium = 12;
+var large = 14;
+var pepperoni = new Meat();
+var sausage = new Meat();
+var ham = new Meat();
+var onion = new Veggie();
+var redPepper = new Veggie();
+var mushroom = new Veggie();
+var garlic = new Veggie();
+var pineapple = new Veggie();
+var jalepeno = new Veggie();
 
 var order = new Orders('Josh');
-var pizza = new Pizza('large');
-
-order.addPizza(pizza);
+var pizza1 = new Pizza(large);
